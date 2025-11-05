f8ca1cffad6e1b76f500cace483fe9f7cd3376a3
Rennn0 <leqterhanibal@gmail.com>
2025-11-05 11:35:13 +0400

    xx

--- Eshop/api/photos.js
+++ Eshop/api/photos.js
@@ -16,8 +16,8 @@ export default async function handler(req, res) {
         });
         const data = await apiResponse.json();
 
-        return res.status(apiResponse.status).json(data);
+        return res.status(apiResponse.status).json({ data });
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        return res.status(500).json({ error });
     }
 }
