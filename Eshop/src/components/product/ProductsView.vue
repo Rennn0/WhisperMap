b94744a67fded4c8a83fa0debdc2aea2540c5ea8
Rennn0 <leqterhanibal@gmail.com>
2025-11-12 13:50:04 +0400

    ZZZ

--- Eshop/src/components/product/ProductsView.vue
+++ Eshop/src/components/product/ProductsView.vue
@@ -21,7 +21,6 @@ const onSelect = (product: Product) => {
 //#region lifecycle hooks
 onUpdated(() => { });
 onMounted(() => {
-    console.log("MOUNT");
     getProducts().then(ps => productsRef.value = ps)
 });
 onUnmounted(() => { })
