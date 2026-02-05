9696121d3c3ff0fd3bfafc64173cf09faa23168d
ldanelia <L.Danelia@betlive.com>
2026-02-05 13:16:07 +0400

    muska

---
+++ XatiCraft/Data/ApplicationContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace XatiCraft.Data;
+
+/// <inheritdoc />
+public class ApplicationContextFactory 
+    : IDesignTimeDbContextFactory<ApplicationContext>
+{
+    /// <inheritdoc />
+    public ApplicationContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        string? connectionString = config.GetConnectionString(nameof(ApplicationContext));
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+            throw new InvalidOperationException($"Connection string {nameof(ApplicationContext)} not found.");
+
+        DbContextOptionsBuilder<ApplicationContext> optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new ApplicationContext(optionsBuilder.Options);
+    }
+}
---
+++ XatiCraft/Migrations/20260205084313_InitalCreate.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace XatiCraft.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<decimal>(type: "numeric(10,3)", precision: 10, scale: 3, nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("products_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product_metadata",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    original_file = table.Column<string>(type: "text", nullable: false),
+                    file_key = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    product_id = table.Column<long>(type: "bigint", nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("product_metadata_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "product_metadata_product_id_fkey",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_metadata_product_id",
+                table: "product_metadata",
+                column: "product_id");
+            
+            migrationBuilder.Sql(@"
+CREATE OR REPLACE VIEW v_products AS
+SELECT
+    p.id,
+    p.title,
+    p.description,
+    p.price,
+    p.""timestamp"",
+    COALESCE(
+        jsonb_agg(
+            jsonb_build_object(
+                'id', pm.id,
+                'original_file', pm.original_file,
+                'file_key', pm.file_key,
+                'location', pm.location,
+                'timestamp', pm.""timestamp""
+            )
+        ) FILTER (WHERE pm.id IS NOT NULL),
+        '[]'::jsonb
+    ) AS metadata
+FROM products p
+LEFT JOIN product_metadata pm ON pm.product_id = p.id
+GROUP BY p.id;
+");
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product_metadata");
+
+            migrationBuilder.DropTable(
+                name: "products");
+            
+            migrationBuilder.Sql("DROP VIEW IF EXISTS v_products;");
+        }
+    }
+}
